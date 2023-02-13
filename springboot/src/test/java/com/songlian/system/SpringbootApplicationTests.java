18931bccea5ded6d5f6a2cf819166ea46aee997e
huangwei-git <1664741919@qq.com>
2023-02-14 05:54:03 +0800

    完成了前端基本框架构建

---
+++ springboot/src/test/java/com/songlian/system/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songlian.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
